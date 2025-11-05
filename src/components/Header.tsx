f1bcc52fd16285e5735681461129e7f2d1d22dd3
MSadawi <sadawe147@gmail.com>
2025-11-06 00:06:44 +0800

    Install and add tailwind css configuration

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from 'react';
+
+export default function Header(): JSX.Element {
+    return (
+        <header className=''>
+            <h1>Header</h1>
+        </header>
+    )
+}
